85383f08deda18b15db7fed8d7fad9ad3b6d302f
sherif17 <sherif17.ahmed@gmail.com>
2021-01-08 10:55:02 +0200

    Light theme applied on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import './screens/login_screens/enter_phone_number.dart';
 import 'screens/login_screens/confirm_is_that_user.dart';
 import 'screens/login_screens/phone_verification.dart';
 import 'screens/login_screens/register_new_user.dart';
+import 'themes/light_theme.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(context) {
     // TODO: implement build
     return new MaterialApp(
+        theme: lightTheme(),
         //home: EnterPhoneNumber(),
         initialRoute: '/PhoneNumber',
         routes: {
