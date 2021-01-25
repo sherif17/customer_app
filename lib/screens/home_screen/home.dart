21a3b9585d1dd044f3eec8037b4c22c5e7609fbc
sherif17 <sherif17.ahmed@gmail.com>
2021-01-26 01:34:53 +0200

    Customer registration almost finished

--- lib/screens/home_screen/home.dart
+++ lib/screens/home_screen/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
 import 'home_body.dart';
 
 class HomeScreen extends StatelessWidget {
