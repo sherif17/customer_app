665d9f8ccffd5f43d9acfd23b18ac22b2a0aa735
sherif17 <sherif17.ahmed@gmail.com>
2021-04-10 22:51:53 +0200

    Preparing to take mechanic screens from vero

---
+++ lib/screens/to_mechanic/mechanic_services.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MechanicService extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MechanicServiceBody(),
+    );
+  }
+}
+
+class MechanicServiceBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
